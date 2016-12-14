c7d9f0181388adc2df1095a181cadc2fad0f4b88
sh4869 <nobuk4869@gmail.com>
2016-12-15 02:11:15 +0900

    Add deploy scripts

---
+++ script/deploy.js
@@ -0,0 +1,24 @@
+const ghpages = require("gh-pages");
+const path = require("path");
+
+const GH_TOKEN = process.env.GH_TOKEN;
+
+ghpages.publish(
+    path.join(__dirname, '..', 'www'),
+    {
+        repo: 'https://' + GH_TOKEN + '@github.com/SIT-Digicre/SIT-Digicre.github.io',
+        user: {
+            name: "sh4869",
+            email: "nobuk4869@gmail.com",
+        },
+        message: "[ci skip] UPDATEE WEBPAGE"
+    },
+    function (err) {
+        if(err){
+            console.log(err);
+            process.exit(1);
+        } else {
+            console.log("Update WebPage");
+        }
+    }
+);
