c5da69e724dfbb543dea09794c4bbf386bfc68c8
sh4869 <nobuk4869@gmail.com>
2016-12-17 22:40:12 +0900

    Add css build setting

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const pug = require("gulp-pug");
 const sass = require("gulp-sass");
+const rework = require("gulp-rework");
+const reworkNPM = require("rework-npm");
+const runSequence = require("run-sequence");
 
 
 gulp.task('pug', () => {
@@ -15,19 +18,29 @@ gulp.task('pug', () => {
         .pipe(gulp.dest("dest/"));
 })
 
-gulp.task("sass",()=>{
-    return gulp.src("www/scss/**/*.scss",{"base": "www/scss"})
-        .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
+gulp.task("rework", () => {
+    return gulp.src("www/scss/import.css")
+        .pipe(rework(reworkNPM()))
+        .pipe(gulp.dest("dest/css/"));
+})
+
+gulp.task("sass", () => {
+    return gulp.src(["www/scss/**/*.scss","!www/scss/import.scss"], { "base": "www/scss" })
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(gulp.dest("dest/css/"));
 });
 
-gulp.task("build", ["pug","sass"], () => {
+gulp.task("css", ["sass","rework"],() => {
+});
+
+gulp.task("build", ["pug", "css"], () => {
 
 });
 
 gulp.task('watch', () => {
     gulp.watch("www/**/*.pug", ["pug"]);
-    gulp.watch("www/scss/**/*.scss",["sass"]);
+    gulp.watch(["www/scss/**/*.scss","!www/scss/import.scss"], ["css"]);
+    gulp.watch("www/scss/import.css",["rework"])
 })
 
 gulp.task('default', ["watch"]);
