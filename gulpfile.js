1fd454338cce6d837b12040cba0bea0ea4786b61
sh4869 <nobuk4869@gmail.com>
2016-12-19 10:35:35 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,8 @@ const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const pug = require("gulp-pug");
 const sass = require("gulp-sass");
-const rework = require("gulp-rework");
-const reworkNPM = require("rework-npm");
 const runSequence = require("run-sequence");
+const moduleImporter = require("sass-module-importer");
 
 
 gulp.task('pug', () => {
@@ -18,29 +17,30 @@ gulp.task('pug', () => {
         .pipe(gulp.dest("dest/"));
 })
 
-gulp.task("rework", () => {
-    return gulp.src("www/scss/import.css")
-        .pipe(rework(reworkNPM()))
-        .pipe(gulp.dest("dest/css/"));
-})
+gulp.task("img", () => {
+    return gulp.src(["www/img/**/*.png", "www/img/**/*.jpg"], { base: "www/img/" })
+        .pipe(gulp.dest("dest/img/"));
+});
 
-gulp.task("sass", () => {
-    return gulp.src(["www/scss/**/*.scss","!www/scss/import.scss"], { "base": "www/scss" })
-        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+gulp.task("css", () => {
+    return gulp.src("www/scss/index.scss", { "base": "www/scss" })
+        .pipe(plumber({
+            errorHandler: (err) => {
+                console.log(err);
+            }
+        }))
+        .pipe(sass({ outputStyle: 'expanded',importer: moduleImporter() }).on('error', sass.logError))
         .pipe(gulp.dest("dest/css/"));
 });
 
-gulp.task("css", ["sass","rework"],() => {
-});
-
-gulp.task("build", ["pug", "css"], () => {
+gulp.task("build", ["img", "pug", "css"], () => {
 
 });
 
 gulp.task('watch', () => {
     gulp.watch("www/**/*.pug", ["pug"]);
-    gulp.watch(["www/scss/**/*.scss","!www/scss/import.scss"], ["css"]);
-    gulp.watch("www/scss/import.css",["rework"])
+    gulp.watch(["www/scss/**/*.scss"], ["css"]);
+    gulp.watch(["www/img/**/*.png", "www/img/**/*.jpg"], ["img"])
 })
 
 gulp.task('default', ["watch"]);
